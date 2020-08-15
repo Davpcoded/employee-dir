81314f0cacf87a11080ffd57692b93b96b7c9c2f
Daniel Villarroel <handonsk82894@gmail.com>
2020-08-14 20:01:14 -0700

    Updates

---
+++ precache-manifest.bcd74c569daa893ff26b4156ab64dc0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5deab21ec66f032afb93dff7256019a4",
+    "url": "/employee-dir/index.html"
+  },
+  {
+    "revision": "b0eeb584988b68870fa8",
+    "url": "/employee-dir/static/js/2.31a137e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-dir/static/js/2.31a137e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eafec805db51e79e6d1",
+    "url": "/employee-dir/static/js/main.8615a5a2.chunk.js"
+  },
+  {
+    "revision": "b8830ef480294ad323d4",
+    "url": "/employee-dir/static/js/runtime-main.5206324e.js"
+  }
+]);
